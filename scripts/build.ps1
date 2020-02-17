62d75d5f5de97664ec7de9d3982618b325599294
Michael Brown <mikeb@360replays.com>
2020-02-17 15:54:56 -0800

    moved build scripts to appveyor

--- scripts/build.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "Building..." -ForegroundColor green
-
-dotnet build .\NUnit.Extension.TestMonitor\NUnit.Extension.TestMonitor.sln | Select-String -Pattern " warning " -NotMatch | Select-String -Pattern "INFO:" -NotMatch | Select-String -Pattern "WARNING:" -NotMatch
-
-Write-Host "Packaging..." -ForegroundColor green
-dotnet pack .\NUnit.Extension.TestMonitor\NUnit.Extension.TestMonitor\NUnit.Extension.TestMonitor.csproj --configuration Release --no-build
-
-Get-ChildItem .\*.nupkg -recurse | % { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
--- scripts/run-tests.ps1
+++
@@ -1,3 +0,0 @@
-Write-Host "Running Tests..." -ForegroundColor green
-
-Get-ChildItem ".\NUnit.Extension.TestMonitor\NUnit.Extension.TestMonitor.Tests\" -recurse | where {$_.extension -eq ".csproj"} | % { dotnet test --no-build --no-restore --test-adapter-path:. --logger:Appveyor /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura $_.FullName }
