bde01e9f83e89de42bc9d03a91f5429c7848428d
Michael Brown <mikeb@360replays.com>
2020-03-07 21:10:49 -0800

    Fixes and updates after heavy testing

--- NUnit.Extension.TestMonitor/NUnit.Extension.TestMonitor.Tests/NUnitCommanderTests.cs
+++ NUnit.Extension.TestMonitor/NUnit.Extension.TestMonitor.Tests/NUnitCommanderTests.cs
@@ -1,24 +1,41 @@
 ﻿using NUnit.Framework;
+using System;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace NUnit.Extension.TestMonitor.Tests
 {
+    /// <summary>
+    /// Tests are designed to test behavior for NUnit.Commander, an external component.
+    /// </summary>
     [TestFixture]
-    public class TestMonitorExtensionTests
+    public class NUnitCommanderTests
     {
         [Test]
         public async Task Should_Test1_Work()
         {
             TestContext.WriteLine($"Running {nameof(Should_Test1_Work)}");
-            await Task.Delay(3 * 1000);
-            Assert.AreEqual(true, true);
+            await Task.Delay(1 * 1000);
+            if (true)
+                Assert.AreEqual(true, true);
+            else
+            {
+                // enable this for random failures
+                using (var rg = new RNGCryptoServiceProvider())
+                {
+                    byte[] rno = new byte[5];
+                    rg.GetBytes(rno);
+                    var val = BitConverter.ToInt32(rno, 0);
+                    Assert.AreEqual(0, val % 2, "Random test failure");
+                }
+            }
         }
 
         [Test]
         public async Task Should_Test2_Work()
         {
             TestContext.WriteLine($"Running {nameof(Should_Test2_Work)}");
-            await Task.Delay(5 * 1000);
+            await Task.Delay(1 * 1000);
             Assert.AreEqual(true, true);
         }
 
@@ -26,7 +43,7 @@ namespace NUnit.Extension.TestMonitor.Tests
         public async Task Should_Test3_Work()
         {
             TestContext.WriteLine($"Running {nameof(Should_Test3_Work)}");
-            await Task.Delay(7 * 1000);
+            await Task.Delay(1 * 1000);
             Assert.AreEqual(true, true);
         }
 
@@ -34,9 +51,9 @@ namespace NUnit.Extension.TestMonitor.Tests
         public async Task Should_Test4_Work()
         {
             TestContext.WriteLine($"Running {nameof(Should_Test3_Work)}");
-            await Task.Delay(3 * 1000);
+            await Task.Delay(1 * 1000);
             Assert.AreEqual(true, true);
-            //Assert.AreEqual(true, false, "Example error message");
+            //Assert.AreEqual(true, false, "Fake error message");
             // throw new System.Exception("This test failed, stack trace is as follows.");
         }
     }
