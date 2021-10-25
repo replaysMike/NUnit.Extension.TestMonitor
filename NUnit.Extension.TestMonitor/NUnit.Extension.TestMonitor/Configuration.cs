5120cbf4410f5c7f812457717e394637ef33bf6e
Michael Brown <mikeb@360replays.com>
2021-10-25 15:13:30 -0700

    enable logging by default

--- NUnit.Extension.TestMonitor/NUnit.Extension.TestMonitor/Configuration.cs
+++ NUnit.Extension.TestMonitor/NUnit.Extension.TestMonitor/Configuration.cs
@@ -10,7 +10,7 @@ namespace NUnit.Extension.TestMonitor
         /// The events to emit
         /// </summary>
         [JsonConverter(typeof(JsonStringEnumConverter))]
-        public EventEmitTypes EventEmitType { get; set; } = EventEmitTypes.NamedPipes | EventEmitTypes.StdOut;
+        public EventEmitTypes EventEmitType { get; set; } = EventEmitTypes.NamedPipes | EventEmitTypes.StdOut | EventEmitTypes.LogFile;
 
         /// <summary>
         /// The format of the event data
